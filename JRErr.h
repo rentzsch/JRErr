23063060d92964acff7333390a1a5e760f419cc1
rentzsch <jwr.git@redshed.net>
2012-10-12 23:29:09 -0500

    [VERSION] 0.0.12

--- JRErr.h
+++ JRErr.h
@@ -1,4 +1,4 @@
-// JRErr.h semver:0.0.11
+// JRErr.h semver:0.0.12
 //   Copyright (c) 2012 Jonathan 'Wolf' Rentzsch: http://rentzsch.com
 //   Some rights reserved: http://opensource.org/licenses/mit
 //   https://github.com/rentzsch/JRErr
