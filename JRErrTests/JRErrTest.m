30af44304916576c44e762efc781d10299c0cca6
rentzsch <jwr.git@redshed.net>
2013-03-29 22:49:40 -0500

    second test passed MRC and ARC

--- JRErrTests/JRErrTest.m
+++ JRErrTests/JRErrTest.m
@@ -59,13 +59,13 @@ static void WeirdErrorAnnotator(const char *codeResultType,
     [[JRErrContext currentContext] popError];
     assert(!jrErr);
     
-#if 0
-    
     assert(!jrErr);
     JRPushErr([obj returnYesAndAnError:jrErrRef]);
     assert(!jrErr);
     [[JRErrContext currentContext] popError];
     assert(!jrErr);
+    
+#if 0
     
     assert(!jrErr);
     JRPushErr([obj returnNoAndNoError:jrErrRef]);
