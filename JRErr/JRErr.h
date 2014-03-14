a4020441c465a75794f57cda08c0fcda2717f400
rentzsch <jwr.git@redshed.net>
2014-03-14 00:15:21 -0500

    [VERSION] 2.0.

--- JRErr/JRErr.h
+++ JRErr/JRErr.h
@@ -1,5 +1,5 @@
-// JRErr.h semver:2.0.0b1
-//   Copyright (c) 2012-2013 Jonathan 'Wolf' Rentzsch: http://rentzsch.com
+// JRErr.h semver:2.0
+//   Copyright (c) 2012-2014 Jonathan 'Wolf' Rentzsch: http://rentzsch.com
 //   Some rights reserved: http://opensource.org/licenses/mit
 //   https://github.com/rentzsch/JRErr
 
